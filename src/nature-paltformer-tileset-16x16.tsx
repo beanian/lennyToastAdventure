618ca03e36a9a969ee9222b117a2caa2441b577f
Ian Bennett <ianbennett24@gmail.com>
2025-08-27 12:36:49 +0100

    added tiles and restructed folder

--- src/nature-paltformer-tileset-16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.11.2" name="nature-paltformer-tileset-16x16" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="assets/tileset_placeholder.png" width="256" height="256"/>
-</tileset>
